6845cbf2eb83a4b0484b4a4621114e1ce28e57ca
alfonso del rio <alfonso.delrio@live.com.mx>
2018-11-28 22:28:44 +0000

    little update

--- sql/sql-cheatsheet.sql
+++ sql/sql-cheatsheet.sql
@@ -7,3 +7,8 @@ mysql> select date_format(from_date, '%d/%m/%Y') as formatted_date from salaries
 mysql> select left('asdf',2);
 mysql> select right('asdf',2);
 mysql> select first_name, last_name, concat(left(first_name,1),left(last_name,1)) as initials from employees limit 10;
+
+-- equals and not equals
+mysql> select * from employees where first_name <> 'Elvis' limit 15;
+mysql> select * from employees where first_name != 'Elvis' limit 15;
+mysql> select * from employees where first_name = 'Elvis' limit 15;
