34c2da85422a9952b31d323fba3c3d092529b9e7
Alfonso del Rio <alfonso.delrio@live.com.mx>
2018-12-04 15:55:59 +0000

    update on joins

--- sql/sql-cheatsheet.sql
+++ sql/sql-cheatsheet.sql
@@ -12,3 +12,8 @@ mysql> select first_name, last_name, concat(left(first_name,1),left(last_name,1)
 mysql> select * from employees where first_name <> 'Elvis' limit 15;
 mysql> select * from employees where first_name != 'Elvis' limit 15;
 mysql> select * from employees where first_name = 'Elvis' limit 15;
+
+-- joins
+-- the same inner and without inner
+mysql> select * from employees inner join dept_manager on employees.emp_no = dept_manager.emp_no limit 10;
+mysql> select * from employees join dept_manager on employees.emp_no = dept_manager.emp_no limit 10;
